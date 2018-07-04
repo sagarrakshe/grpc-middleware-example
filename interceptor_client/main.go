790d7111e2ac192b5d8c1f261dcc359fa90bb7d9
sagarrakshe <sagarrakshe2@gmail.com>
2018-07-05 00:23:41 +0700

    Renamed the grpc_retry module alias

--- interceptor_client/main.go
+++ interceptor_client/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"time"
 
-	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
+	grpcRetry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
 	ts "github.com/sagarrakshe/grpc-middleware-example/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -13,14 +13,14 @@ import (
 )
 
 func main() {
-	opts := []grpc_retry.CallOption{
-		grpc_retry.WithBackoff(grpc_retry.BackoffLinear(10 * time.Second)),
-		grpc_retry.WithCodes(codes.NotFound, codes.Aborted, codes.Unavailable, codes.Unknown, codes.ResourceExhausted, codes.Unavailable),
+	opts := []grpcRetry.CallOption{
+		grpcRetry.WithBackoff(grpcRetry.BackoffLinear(10 * time.Second)),
+		grpcRetry.WithCodes(codes.NotFound, codes.Aborted, codes.Unavailable, codes.Unknown, codes.ResourceExhausted, codes.Unavailable),
 	}
 
 	conn, err := grpc.Dial(":8001",
 		grpc.WithInsecure(),
-		grpc.WithUnaryInterceptor(grpc_retry.UnaryClientInterceptor(opts...)),
+		grpc.WithUnaryInterceptor(grpcRetry.UnaryClientInterceptor(opts...)),
 	)
 	if err != nil {
 		panic(err)
@@ -34,7 +34,7 @@ func main() {
 
 	res, err := c.SayHello(ctx, &ts.HelloRequest{
 		Name: "World!",
-	}, grpc_retry.WithMax(5), grpc_retry.WithPerRetryTimeout(10*time.Second))
+	}, grpcRetry.WithMax(5), grpcRetry.WithPerRetryTimeout(10*time.Second))
 
 	if err != nil {
 		panic(err)
@@ -45,7 +45,7 @@ func main() {
 	// Second Request
 	resp, err := c.SayHello(ctx, &ts.HelloRequest{
 		Name: "New World!",
-	}, grpc_retry.WithMax(3), grpc_retry.WithPerRetryTimeout(1*time.Second))
+	}, grpcRetry.WithMax(3), grpcRetry.WithPerRetryTimeout(1*time.Second))
 
 	if err != nil {
 		panic(err)
